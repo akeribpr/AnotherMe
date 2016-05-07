6c1737b2a2911055b6b864ab90286a6447098171
or akerib <or akerib>
2016-05-07 18:23:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/oris1991/anotherme/CalendarViewFragment.java
@@ -0,0 +1,275 @@
+package com.example.oris1991.anotherme;
+
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+/**
+ * Created by oris1991 on 07/05/2016.
+ */
+public class CalendarViewFragment extends Fragment{
+
+    interface Delegate{
+       // public Runnable calendarUpdater();
+    }
+
+    public GregorianCalendar month, itemmonth;// main_content instances.
+
+    public CalendarAdapter adapter;// adapter instance
+    public Handler handler;// for grabbing some event values for showing the dot
+    // marker.
+    public ArrayList<String> items; // container to store main_content items which
+    // needs showing the event marker
+    ArrayList<String> event;
+    ListView rLayout;
+    ArrayList<String> date;
+    ArrayList<String> desc;
+    ArrayList<Integer> time;
+    MyAddapter adapterEvent = new MyAddapter();
+    View view;
+    Context mContext;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.calendar_fragment, container, false);
+
+        mContext=container.getContext();
+
+
+        Locale.setDefault(Locale.US);
+
+        rLayout = (ListView) view.findViewById(R.id.event_listview);
+        month = (GregorianCalendar) GregorianCalendar.getInstance();
+        itemmonth = (GregorianCalendar) month.clone();
+
+        items = new ArrayList<String>();
+
+        adapter = new CalendarAdapter(getActivity(), month);
+
+        GridView gridview = (GridView) view.findViewById(R.id.gridview);
+        gridview.setAdapter(adapter);
+
+        handler = new Handler();
+        handler.post(calendarUpdater);
+
+        TextView title = (TextView) view.findViewById(R.id.title);
+        title.setText(android.text.format.DateFormat.format("MMMM yyyy", month));
+
+        ImageView previous = (ImageView) view.findViewById(R.id.previous);
+
+        previous.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setPreviousMonth();
+                refreshCalendar();
+            }
+        });
+
+        ImageView next = (ImageView) view.findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setNextMonth();
+                refreshCalendar();
+
+            }
+        });
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                // removing the previous view if added
+             /*  if (((ListView) rLayout).getChildCount() > 0) {
+                    ((ListView) rLayout).removeAllViews();
+                }*/
+                desc = new ArrayList<String>();
+                date = new ArrayList<String>();
+                time =new ArrayList<Integer>();
+
+
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v);
+                String selectedGridDate = CalendarAdapter.dayString
+                        .get(position);
+                String[] separatedTime = selectedGridDate.split("-");
+                String gridvalueString = separatedTime[2].replaceFirst("^0*",
+                        "");// taking last part of date. ie; 2 from 2012-12-02.
+                int gridvalue = Integer.parseInt(gridvalueString);
+                // navigate to next or previous month on clicking offdays.
+                if ((gridvalue > 10) && (position < 8)) {
+                    setPreviousMonth();
+                    refreshCalendar();
+                } else if ((gridvalue < 7) && (position > 28)) {
+                    setNextMonth();
+                    refreshCalendar();
+                }
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v);
+
+                for (int i = 0; i < Utility.startDates.size(); i++) {
+                    if (Utility.startDates.get(i).equals(selectedGridDate)) {
+                        time.add(i);
+                    }
+                }
+
+                int temp;
+                for (int i=0;i<time.size();i++)
+                    for (int j=0+i;j<time.size();j++)
+                    {
+                        if (Utility.startTime.get(time.get(i)).compareTo(Utility.startTime.get(time.get(j)))>0)
+                        {
+                            temp=time.get(i);
+                            time.set(i,time.get(j));
+                            time.set(j,temp);
+                        }
+                    }
+                for (int i=0;i<time.size();i++)
+                {
+                    desc.add(Utility.nameOfEvent.get(time.get(i))+"  "+Utility.startTime.get(time.get(i)));
+                }
+
+                rLayout.setAdapter(adapterEvent);
+
+
+            /*    if (desc.size() > 0) {
+                    for (int i = 0; i < desc.size(); i++) {
+                        TextView rowTextView = new TextView(MainActivity.this);
+
+                        // set some properties of rowTextView or something
+                        rowTextView.setText("Event:" + desc.get(i));
+                        rowTextView.setTextColor(Color.BLACK);
+
+
+                        // add the textview to the linearlayout
+                        //rLayout.addView(rowTextView);
+                        rLayout.setAdapter(adapter);
+
+                    }
+
+                }*/
+
+                // desc = null;
+
+            }
+
+        });
+        return view;
+    }
+
+    protected void setNextMonth() {
+        if (month.get(GregorianCalendar.MONTH) == month
+                .getActualMaximum(GregorianCalendar.MONTH)) {
+            month.set((month.get(GregorianCalendar.YEAR) + 1),
+                    month.getActualMinimum(GregorianCalendar.MONTH), 1);
+        } else {
+            month.set(GregorianCalendar.MONTH,
+                    month.get(GregorianCalendar.MONTH) + 1);
+        }
+
+    }
+
+    protected void setPreviousMonth() {
+        if (month.get(GregorianCalendar.MONTH) == month
+                .getActualMinimum(GregorianCalendar.MONTH)) {
+            month.set((month.get(GregorianCalendar.YEAR) - 1),
+                    month.getActualMaximum(GregorianCalendar.MONTH), 1);
+        } else {
+            month.set(GregorianCalendar.MONTH,
+                    month.get(GregorianCalendar.MONTH) - 1);
+        }
+
+    }
+
+    public void refreshCalendar() {
+        TextView title = (TextView) view.findViewById(R.id.title);
+
+        adapter.refreshDays();
+        adapter.notifyDataSetChanged();
+        handler.post(calendarUpdater); // generate some main_content items
+
+        title.setText(android.text.format.DateFormat.format("MMMM yyyy", month));
+    }
+
+    public Runnable calendarUpdater = new Runnable() {
+
+        @Override
+        public void run() {
+            items.clear();
+
+            // Print dates of the current week
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+            String itemvalue;
+            event = Utility.readCalendarEvent(getActivity().getApplicationContext());
+            Log.d("=====Event====", event.toString());
+            Log.d("=====Date ARRAY====", Utility.startDates.toString());
+
+            for (int i = 0; i < Utility.startDates.size(); i++) {
+                itemvalue = df.format(itemmonth.getTime());
+                itemmonth.add(GregorianCalendar.DATE, 1);
+                items.add(Utility.startDates.get(i).toString());
+            }
+            adapter.setItems(items);
+            adapter.notifyDataSetChanged();
+        }
+    };
+
+    class MyAddapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            if (desc!=null)
+                return desc.size();
+            else
+                return 0;
+
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return desc.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+            if(convertView == null){
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                convertView = inflater.inflate(R.layout.event_row, null);
+            }
+            TextView eventName = (TextView) convertView.findViewById(R.id.event_text);
+            eventName.setText(desc.get(position));
+            return convertView;
+        }
+    }
+}
