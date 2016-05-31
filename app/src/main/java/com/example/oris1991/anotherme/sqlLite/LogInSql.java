f2f984622a0061eeaf1760c45ca249373711ce74
Itzik <itzikc2.ic@gmail.com>
2016-05-31 17:42:40 +0300

    task

--- app/src/main/java/com/example/oris1991/anotherme/sqlLite/LogInSql.java
+++ app/src/main/java/com/example/oris1991/anotherme/sqlLite/LogInSql.java
@@ -32,15 +32,32 @@ public class LogInSql {
             Cursor cursor = db.query(LOGIN_TABLE, null, null, null, null, null, null);
 
 
-        if (cursor.moveToFirst()) {
-            int personId = cursor.getColumnIndex(PERSON_ID);
-            int password = cursor.getColumnIndex(PASSWORD);
-
-            logIn = new LogIn(cursor.getString(personId),cursor.getString(password));
-            return logIn;
+        if (!(cursor.moveToFirst()) || cursor.getCount() ==0){
+            return logIn = new LogIn("1","1");
         }
+
+            if (cursor.moveToFirst()) {
+                int personId = cursor.getColumnIndex(PERSON_ID);
+                int password = cursor.getColumnIndex(PASSWORD);
+
+                logIn = new LogIn(cursor.getString(personId), cursor.getString(password));
+                return logIn;
+            }
+
         return logIn;
     }
+
+//    public static boolean tableExists(SQLiteDatabase db){
+//        Cursor cursor = db.query("SELECT name FROM sqlite_master WHERE type='table' AND name='"+LOGIN_TABLE+"'");
+//
+//
+//        if (cursor.moveToFirst()) {
+//            int personId = cursor.getColumnIndex(PERSON_ID);
+//            int password = cursor.getColumnIndex(PASSWORD);
+//
+//        return true
+//
+//    }
 
     public static boolean checkUser(SQLiteDatabase db, LogIn sp) {
 
@@ -48,6 +65,9 @@ public class LogInSql {
         Log.d("Log",logData.getPersonId() +"    "+logData.getPassword());
         Log.d("Log",sp.getPersonId() +"    "+sp.getPassword());
 
+//        if(logData==null){
+//            return false;
+//        }
         if ((logData.getPersonId().equals(sp.getPersonId()))&&(logData.getPassword().equals(sp.getPassword()))) {
             return true;
         } else {
