15668ed1f43cbfbeb9feb71535656b81cb97f324
Itzik <itzikc2.ic@gmail.com>
2016-05-31 11:32:17 +0300

    pic

---
+++ app/src/main/java/com/example/oris1991/anotherme/Model/SharePictureOrText.java
@@ -0,0 +1,16 @@
+package com.example.oris1991.anotherme.Model;
+
+/**
+ * Created by Itzik on 31/05/2016.
+ */
+public class SharePictureOrText {
+
+    int id;
+    String PicName;
+    String ShardWtith;
+    String text;
+    Boolean Send;
+
+
+
+}
